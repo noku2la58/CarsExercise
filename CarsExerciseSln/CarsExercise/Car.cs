3118340e8d7a4905b213880183acaba44f4417dc
noku2la58 <4275819@myuwc.ac.za>
2022-04-04 14:30:04 +0200

    Add project files.

---
+++ CarsExerciseSln/CarsExercise/Car.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsExercise
+{
+    public class Car
+    {
+        private int _year;
+        private string _make;
+        private int _speed;
+
+        private int _tirePressure;
+
+        public string Colour;
+
+        public Car()
+        {
+            this._year = 1994;
+            this._make = "Ford";
+            this._speed = 0;
+
+            Console.WriteLine("");
+        }
+
+        public Car(int year, string make, int speed)
+        {
+            this._year = year;
+            this._make = make;
+            this._speed = speed;
+        }
+
+        public string Make
+        {
+            get
+            {
+                return _make;
+            }
+
+            set
+            {
+                _make = value;
+            }
+        }
+
+        public int Year
+        {
+            get { return _year; }
+            set { _year = value; }
+        }
+
+        public int Speed
+        {
+            get { return _speed; }
+            set { _speed = value; }
+        }
+
+        public void DriveFaster(int increment)
+        {
+            _speed += increment;
+
+        }
+        public void DriveSlower(int increment)
+        {
+            _speed -= increment;
+        }
+
+
+        public string GetMake()
+        {
+            return _make;
+        }
+
+        public void SetMake(string make)
+        {
+            _make = make;
+        }
+
+        public int GetTirePressure()
+        {
+            return _tirePressure;
+        }
+
+    }
+
+
+  
+
+
+}
---
+++ CarsExerciseSln/CarsExercise/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CarsExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car(2003, "Jazz", 0);
+            myCar.Colour = "Red";
+            myCar.DriveFaster(20);
+           
+            Car angelsCar = new Car(2022, "Porsche", 0);
+            angelsCar.Colour = "Pink";
+            angelsCar.DriveFaster(120);
+
+            Console.WriteLine("Allans Jazz car: " + myCar.Speed);
+            Console.WriteLine("Angels Pink car: " + angelsCar.Speed);
+           
+            /*
+            
+            Car vw = new Car(2020, "Polo", 10);
+            vw.DriveFaster(45);
+            vw.Make = "Golf";
+
+            Car bmw = new Car(2019, "M3", 0);
+
+            Car rollsRoyce = new Car(2019, "Phantom", 0);
+
+            Car mazda = new Car(1989, "323", 0);
+
+            Car toyota = new Car(1973, "Cresida", 0);
+
+            */
+        }
+    }
+}
